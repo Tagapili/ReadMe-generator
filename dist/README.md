d28564d1a5cca0739e07fc428085aa37180fd37b
mj caballero <mj.caballero@yahoo.com>
2023-11-27 04:14:35 -0600

    finishing touches

--- dist/README.md
+++ dist/README.md
@@ -1,2 +1,32 @@
-# undefined
+### ReadMe Generator
 
+  ## License
+  ![Github license](https://img.shields.io/badge/license-MIT-yellowgreen.svg)
+  
+  ## Description
+  This app gives the readme creation a faster and structured process.
+
+  ## Table of Contents
+  * [License](#license)
+  * [Description](#description)
+  * [Instructions](#instructions)
+  * [Contribution_Guidelines](#guidelines)
+  * [Test_Instructions](#test)
+
+
+  ## Instructions
+
+  First to run the app type in bash terminal node index.js. Then give the project a name, and project description. Next is user will have to pick a license from a multiple choice.Then user wil have to give the instructions on using the app. Then User will give Contribution Guidelines and Give test instructions.
+
+  ## Contribution Guidelines
+  
+  
+
+  ## Test_Instructions 
+
+  
+  
+  Pull requests are welcome. For major changes, please open an issue first
+  to discuss what you would like to change.
+  
+  Please make sure to update tests as appropriate.
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,30 +1,64 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(license !== "none") {
+    return `\n* [license](#license)\n`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "none"){
+    
+    return `## license
+    
+        licensed under the ${license} license.`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `### ${data.title}
-  
-  ${data.discription}
 
-  ## INSTRUCTIONS
+  ## License
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [License](#license)
+  * [Description](#description)
+  * [Instructions](#instructions)
+  * [Contribution_Guidelines](#guidelines)
+  * [Test_Instructions](#test)
+
+
+  ## Instructions
 
   ${data.instructions}
 
-  ## CONTRIBUTING GUIDELINES
+  ## Contribution Guidelines
   
-  ${data.guidlines}
+  ${data.guidelines}
 
-  ## TEST INSTRUCTIONS 
+  ## Links for Deployment
+
+  ${data.deployment}
+
+  ## Test_Instructions 
 
   ${data.test}
   
@@ -32,10 +66,6 @@ function generateMarkdown(data) {
   to discuss what you would like to change.
   
   Please make sure to update tests as appropriate.
-  
-  ## License
-  ${data.license}
-
 `
 }
 
